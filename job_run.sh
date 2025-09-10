53420621b57bb104892149d168b7bf07c112c6e7
Vivek Sapkal <viveksapkal2003@gmail.com>
2025-09-11 01:06:53 +0530

    minor changes

--- job_run.sh
+++ job_run.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=test              # GPU partition
 #SBATCH --cpus-per-task=12           # CPUs for dataloader + decoding
 #SBATCH --mem-per-cpu=2G             # 2 GB per CPU core = 24 GB total
-#SBATCH --time=00:10:00              # Walltime
+#SBATCH --time=24:00:00              # Walltime
 #SBATCH --nodes=1                    # Single node
 #SBATCH --ntasks=1                   # Run a single task
 #SBATCH -D /scratch/data/bikash_rs/vivek/MELD-feature-extract   # Working directory (important!)
@@ -18,13 +18,7 @@ module load python/3.10
 source venv/bin/activate
 
 # Sanity check GPU
-python gpu_check.py
+# python gpu_check.py
 
 # Run extraction
-# python extract_mae_embedding.py \
-#     --dataset 'MELD' \
-#     --video_dir "/scratch/data/bikash_rs/vivek/dataset/MELD.Raw/train_splits" \
-#     --save_dir "/scratch/data/bikash_rs/vivek/dataset/Meld_feat_ext/train_mae_feat" \
-#     --pretrain_model "mae_checkpoint-340" \
-#     --device cuda \
-#     --feature_level UTTERANCE
+python extract_mae_embedding.py --dataset 'MELD' --video_dir "/scratch/data/bikash_rs/vivek/dataset/MELD.Raw/train_splits" --save_dir "/scratch/data/bikash_rs/vivek/dataset/Meld_feat_ext/train_mae_feat" --pretrain_model "mae_checkpoint-340" --device cuda --feature_level UTTERANCE
